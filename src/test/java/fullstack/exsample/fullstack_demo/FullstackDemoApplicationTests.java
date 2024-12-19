2167694cbd7c2fcfbbbe9005960d61ce70079735
Peter Bilberg <peter.langbo.bilberg.beumer@gmail.com>
2024-12-19 16:43:06 +0100

    full stack demo project clean spring project

---
+++ src/test/java/fullstack/exsample/fullstack_demo/FullstackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fullstack.exsample.fullstack_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
